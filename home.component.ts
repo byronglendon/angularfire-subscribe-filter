60eaad2a3f15ee343480bd3091c9760c3010adb5
Byron <byronglendon@gmail.com>
2019-09-24 18:43:45 +1200

    Update home.component.ts

--- home.component.ts
+++ home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ProjectService, ProjectInterface } from '@app/services/project.service';
 import { ProjectInterface } from '@app/data-models/project.model';
 @Component({
@@ -6,11 +6,13 @@ import { ProjectInterface } from '@app/data-models/project.model';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   activeProjects: Array<ProjectInterface>;
   completedProjects: Array<ProjectInterface>;
   archivedProjects: Array<ProjectInterface>;
 
+  private unsubscribe: Subject<void> = new Subject();
+  
   constructor(
     private projectService: ProjectService
   ) {}
@@ -25,5 +27,11 @@ export class HomeComponent implements OnInit {
       this.completedProjects = resObj.filter(project => project.status === 'completed' );
       this.archivedProjects = resObj.filter(project => project.status === 'archived' );
     });
+  }
+  
+  ngOnDestroy() {
+    console.log('ngOnDestory');
+    this.unsubscribe.next();
+    this.unsubscribe.complete();
   }
 }
